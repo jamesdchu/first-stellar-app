f0ad76c7f3558d1643b3726211ad2639af6de960
jameschu <jameschu@umich.edu>
2021-01-11 00:14:43 -0500

    Testing connections

---
+++ stellar-app/cryptoAPI.py
@@ -0,0 +1,25 @@
+from requests import Request, Session 
+from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
+import json
+
+url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+parameters = {
+  'start':'1',
+  'limit':'5000',
+  'convert':'USD'
+}
+headers = {
+  'Accepts': 'application/json',
+  'X-CMC_PRO_API_KEY': 'c38912a6-bb91-453e-956f-a0a11487a6f4',
+}
+
+session = Session()
+session.headers.update(headers)
+
+try:
+  response = session.get(url, params=parameters)
+  data = json.loads(response.text)
+  print(data)
+except (ConnectionError, Timeout, TooManyRedirects) as e:
+  print(e)
+
